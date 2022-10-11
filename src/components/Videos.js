b1d0c98b92ad76bc7b86f5128ad424d289014da9
Yu <zyken9054@gmail.com>
2022-10-11 11:15:11 -0400

    related video section complete on video detail page, ready to deploy

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,13 +1,12 @@
-import { InsertEmoticon } from '@mui/icons-material'
 import { Stack, Box } from '@mui/material'
 
-import { VideoCard, ChannelCard } from './'
+import { VideoCard, Loader, ChannelCard } from './'
 
-const Videos = ({ videos }) => {
-
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
 
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, i) => (
         <Box key={i}>
           {item.id.videoId && <VideoCard video={item}/>}
