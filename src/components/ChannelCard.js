874601b39502d87ebcb21984beda9b20382946bf
Yu <zyken9054@gmail.com>
2022-10-11 09:03:15 -0400

    finished with channel profile page

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
     return (
         <Box
             sx={{
@@ -13,21 +13,22 @@ const ChannelCard = ({ channelDetail }) => {
                 display: 'flex',
                 justifyContent: 'center',
                 alighItems: 'center',
-                width: { xs: '356px', md: '320px'},
+                width: { xs: '356px', md: '320px' },
                 height: '326px',
-                margin: 'auto'
+                margin: 'auto',
+                marginTop: marginTop
             }}
         >
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent:'center', textAlign: 'center', color: '#fff'}}>
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
                     <CardMedia
                         image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
                         alt={channelDetail?.snippet?.title}
                         sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
                     />
                     <Typography variant="h6">
-                        { channelDetail?.snippet?.title}
-                        <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px'}} />
+                        {channelDetail?.snippet?.title}
+                        <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
                     </Typography>
                     {channelDetail?.statistics?.subscriberCount && (
                         <Typography>
