a5ba5023f8d6cc2d3cb110affcf1e3d608418396
Yu <zyken9054@gmail.com>
2022-10-10 22:15:16 -0400

    finished with VideoCard and ChannelCard components

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -17,7 +17,7 @@ const Feed = () => {
     }, [selectedCategory]);
 
     return (
-        <Stack sx={{ flexDirection: { sx: 'column', md: 'row'}}}>
+        <Stack sx={{ flexDirection: { xs: 'column', md: 'row'}}}>
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
                 <Sidebar 
                     selectedCategory={selectedCategory}
